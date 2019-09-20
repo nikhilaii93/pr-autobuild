ba0f44a9f6f0c23d62ff53753850ff6fcb126445
Nikhil <nikhil@bidgely.com>
2019-09-20 12:53:13 +0530

    Update array assignment

--- bin/util-methods.sh
+++ bin/util-methods.sh
@@ -54,8 +54,8 @@ function check_labels () {
     local match
 	match=false
     local labels=()
-    # shellcheck disable=SC2178
-    labels=$1
+    
+    labels=( "$1" )
     
 	for row in $(echo "${labels[@]}" | jq -r '.[] | @base64'); do
     	_jq() {
